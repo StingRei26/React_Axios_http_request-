ce20016be6fb9293579fe1597aca5107766a7010
Reiniel Ravelo <reinielravelo@gmail.com>
2020-01-02 12:41:36 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
+      // BR is used to add routing to react projects
+      <BrowserRouter>
       <div className="App">
         <Blog />
       </div>
+      </BrowserRouter>
     );
   }
 }
